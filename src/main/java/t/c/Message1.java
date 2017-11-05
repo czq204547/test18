8e59315f8a0f68a6fb5a6b215e316da3bca64a71
czq204547 <plmmlp17@163.com>
2017-11-05 23:09:38 +0800

    1
    1

---
+++ src/main/java/t/c/Message1.java
@@ -0,0 +1,47 @@
+package t.c;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Administrator on 2017/10/29.
+ */
+//返回json数据
+public class Message1 {
+    private int code;
+    private String message;
+    private boolean result;
+
+    public Message1() {
+    }
+
+    public Message1(int code, String message, boolean result) {
+        this.code = code;
+        this.message = message;
+        this.result = result;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isResult() {
+        return result;
+    }
+
+    public void setResult(boolean result) {
+        this.result = result;
+    }
+
+}
